2d038cb5413cd6bc5d8f8f7cef074d3f10ddae47
atari-monk <atari.monk1@gmail.com>
2024-12-09 17:16:57 +0100

    apps, libs generated

---
+++ apps/client/src/app/app.tsx
@@ -0,0 +1,14 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <style jsx>{`
+        /** your style here **/
+      `}</style>
+      <NxWelcome title="client" />
+    </div>
+  );
+}
+
+export default App;
