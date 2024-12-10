a3aee0763e267efeb5a68b9619fa02b6dfd1cf09
atari-monk <atari.monk1@gmail.com>
2024-12-10 19:15:32 +0100

    status ui move

--- packages/shared-ui/src/index.ts
+++ packages/shared-ui/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/shared-ui';
-export { default as SocketStatus } from './lib/socket-status';
+export { default as SocketStatus } from './status/socket-status';
 export { default as Chat } from './chat/chat';
 export { default as GameGrid } from './battleship/grid';
