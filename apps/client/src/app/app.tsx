dace363784c56efbfc4fc4df0b0c59a95bceaaaa
atari-monk <atari.monk1@gmail.com>
2024-12-10 18:03:20 +0100

    fix error

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -3,9 +3,6 @@ import { Chat, GameGrid, SocketStatus } from '@nx-web-test/shared-ui';
 export function App() {
   return (
     <div>
-      <style jsx>{`
-        /** your style here **/
-      `}</style>
       {/* <GameGrid gridSize={10} /> */}
       {/* <Chat /> */}
       <SocketStatus socketUrl="http://localhost:3333" />
